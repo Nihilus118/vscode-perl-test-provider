0a1bafb385e3f5eb3f43670564ed70bbced24f5f
Nihilus118 <johannes.ritter.ma@gmail.com>
2023-08-06 09:45:18 +0200

    add simple perl tests

---
+++ sample/test.pl
@@ -0,0 +1,9 @@
+use warnings;
+use strict;
+use Test::Simple tests => 3;
+
+ok( 1 + 1 == 2, "add one plus one" );
+
+ok( 2 + 2 == 4, "add two plus two" );
+
+ok( 3 + 3 == 5, "add three plus three" );
